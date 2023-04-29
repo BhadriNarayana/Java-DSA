f84369d169b6f4aa183f45b2acfacba8b9ba53bd
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-04-29 18:34:50 +0530

    Idea of efficient apporach for rotating matrix by 90

---
+++ src/multidimensionalArrays/RotateBy90Eff.java
@@ -0,0 +1,12 @@
+package multidimensionalArrays;
+
+public class RotateBy90Eff {
+	static void rotateBy90Eff(int[][] arr, int r, int c) {
+		//step - 1 : Transpose
+		//step -2:  Reverse the rows
+	}
+	public static void main(String args[]) {
+		
+	}
+
+}
