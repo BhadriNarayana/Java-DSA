f5210cff8a721a347bd1e30b5d61b7b3af807819
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-05-08 12:57:15 +0530

    Number of unique Elements

--- src/hashing/NoOfUniqueElements.java
+++ src/hashing/NoOfUniqueElements.java
@@ -8,8 +8,9 @@ public class NoOfUniqueElements {
 		for(int i = 0; i < arr.length; i++) {
 			set.add(arr[i]);
 		}
-		
+	
 		System.out.println(set);
+		System.out.println("Number of unique elements: " + set.size());
 	}
 	public static void main(String args[]) {
 		int[] arr = {1, 2, 3, 4, 5, 5, 8, 2, 1};
