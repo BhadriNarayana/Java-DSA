c10dceb178691b993ecd2592918062b14a889837
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-05-08 12:54:09 +0530

    UniqueElements

---
+++ src/hashing/NoOfUniqueElements.java
@@ -0,0 +1,18 @@
+package hashing;
+import java.util.HashSet;
+public class NoOfUniqueElements {
+	
+	static void uniqueElements(int[] arr) {
+		HashSet<Integer> set = new HashSet<Integer>(arr.length);
+		
+		for(int i = 0; i < arr.length; i++) {
+			set.add(arr[i]);
+		}
+		
+		System.out.println(set);
+	}
+	public static void main(String args[]) {
+		int[] arr = {1, 2, 3, 4, 5, 5, 8, 2, 1};
+		uniqueElements(arr);
+	}
+}
